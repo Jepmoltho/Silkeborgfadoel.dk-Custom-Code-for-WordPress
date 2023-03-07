c5f9847b4e284b540bcd03cb6424cbea89e151b4
Jepmoltho <jept@itu.dk>
2023-03-07 18:36:23 +0100

    JS Quicksave

--- customJS.js
+++ customJS.js
@@ -1,3 +1,14 @@
+function removeMissionStatementOnMobile() {
+  if (document.location.href.length < 30) {
+    document.querySelector(".site-branding").style.paddingBottom = "22px";
+  } else if (window.innerWidth < 800) {
+    document.querySelector(".site-description").style.display = "none";
+    document.querySelector(".entry-header").style.display = "none";
+    document.querySelector(".site-branding").style.paddingBottom = "0px";
+    document.querySelector(".site-content").style.paddingTop = "0px";
+  }
+}
+
 function hideWidgetsOnNewProductPage() {
   if (document.location.href.includes("page_id=1856")) {
     document.querySelector(".widget_media_gallery").style.display = "none";
@@ -17,35 +28,37 @@ function hideWidgetsOnOldProductPage() {
 }
 
 function hideWidgetsOnSofticePage() {
+  if (document.location.href.includes("softice")) {
+    document.querySelector(".widget_media_gallery").style.display = "none";
+    document.querySelector(".widget_contact_info").style.display = "none";
+    document.querySelector(".widget-title").style.marginTop = "0px";
+  }
+}
+
+function hideWidgetsOnTuk() {
   if (document.location.href.includes("tuk-tuk")) {
     document.querySelector(".widget_media_gallery").style.display = "none";
     document.querySelector(".widget_contact_info").style.display = "none";
   }
 }
 
-function hideWidgetsOnTuk() {
+function hideWidgetsOnFestAndGames() {
   if (document.location.href.includes("fest")) {
     document.querySelector(".widget_media_gallery").style.display = "none";
     document.querySelector(".widget_contact_info").style.display = "none";
   }
 }
 
-function hideWidgetsOnFestAndGames() {
+function hideWidgetsOnContact() {
   if (document.location.href.includes("kontakt")) {
     document.querySelector(".widget_media_gallery").style.display = "none";
-    document.querySelector(".widget_contact_info").style.display = "none";
-  }
-}
-
-function hideWidgetsOnContact() {
-  if (document.location.href.includes("om-os")) {
     document.getElementById("block-2").style.display = "none";
     document.getElementById("block-7").style.display = "none";
   }
 }
 
 function hideWidgetsOnAbout() {
-  if (document.location.href.includes("nyheder")) {
+  if (document.location.href.includes("om-os")) {
     document.querySelector(".widget_media_gallery").style.display = "none";
     document.querySelector(".widget_contact_info").style.display = "none";
   }
@@ -59,6 +72,7 @@ function hideWidgetsOnNews() {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
+  removeMissionStatementOnMobile();
   hideWidgetsOnNewProductPage();
   hideWidgetsOnOldProductPage();
   hideWidgetsOnSofticePage();
